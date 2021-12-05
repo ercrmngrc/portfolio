cedf6e5913fcf0fe3b08fb4acb26cc9a1c4e0d97
Eric Garcia <erraga09@gmail.com>
2021-12-05 16:41:36 -0700

    topbar updated

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import "./topbar.scss"
-import { Person, Mail } from "@material-ui/icons"
+import {Person, Mail} from "@material-ui/icons"
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className = "topbar">
+        <div className = {"topbar " + (menuOpen && "active")}>
             <div className = "wrapper">
                 <div className = "left">
-                    <a href = "#intro" className = "logo">genius</a>
+                    <a href = "#intro" className = "logo">Luffy</a>
                     <div className = "itemContainer">
                         <Person className = "icon" />
-                        <span>+9835 9484 3424</span>
+                        <span>+35 9484 3424</span>
                     </div>
                     <div className = "itemContainer">
                         <Mail className = "icon" />
@@ -19,7 +19,11 @@ export default function Topbar() {
                 </div>
                 
                 <div className = "right">
-                
+                    <div className = "hamburger" onClick = {() => setMenuOpen(!menuOpen)}>
+                        <span className = "line1"></span>
+                        <span className = "line2"></span>
+                        <span className = "line3"></span>
+                    </div>
                 </div>
             </div>
             
