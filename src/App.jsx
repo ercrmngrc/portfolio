d729e3e90087362a0827bdf829229feba2deb376
Eric Garcia <erraga09@gmail.com>
2021-12-06 15:27:28 -0700

    added menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import "./app.scss"
 import { useState } from "react";
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className = "app">
       <Topbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
+      <Menu menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
       <div className =" sections">
         <Intro />
         <Portfolio />
