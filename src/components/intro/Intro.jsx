87b80b7dde6115f54f8d097d9c6d5cf91458fab1
Eric Garcia <erraga09@gmail.com>
2021-12-07 12:02:39 -0700

    intro page and added assets folder

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className = "intro" id = "intro">
-            
+            <div className = "left">
+                <div className = "imgContainer">
+                    <img src = "assets/luffy1.png" alt = ""/>
+                </div>
+            </div>
+            <div className = "right">
+                <div className = "wrapper">
+                    <h2>Hi There I'm</h2>
+                    <h1>Monkey D. Luffy</h1>
+                    <h3>King of The Pirates<span></span></h3>
+                </div>
+                <a href = "#portfolio">
+                    <img src = "assets/downArrow.png" alt = ""/>
+                </a>
+            </div>
         </div>
     )
 }
