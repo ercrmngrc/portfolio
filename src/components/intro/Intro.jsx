7908a1da8fd491c43a73a54edac89ee7abc4e8a6
Eric Garcia <erraga09@gmail.com>
2021-12-07 21:17:38 -0700

    finished intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+    const textRef = useRef();
+    
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true, 
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["The King of the", "Pirates"], 
+        });
+    }, []);
+    
     return (
         <div className = "intro" id = "intro">
             <div className = "left">
@@ -13,7 +26,7 @@ export default function Intro() {
                 <div className = "wrapper">
                     <h2>Hi There I'm</h2>
                     <h1>Monkey D. Luffy</h1>
-                    <h3>King of The Pirates<span></span></h3>
+                    <h3>I'm gonna become <span ref = {textRef}></span></h3>
                 </div>
                 <a href = "#portfolio">
                     <img src = "assets/downArrow.png" alt = ""/>
